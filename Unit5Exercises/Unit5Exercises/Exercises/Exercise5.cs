96b110ec2eb5f622078ab9a4cb26bb8fd08777ae
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-28 12:59:51 +0100

    Exercise 5

--- Unit5Exercises/Unit5Exercises/Exercises/Exercise5.cs
+++ Unit5Exercises/Unit5Exercises/Exercises/Exercise5.cs
@@ -13,7 +13,31 @@ namespace ExerciseUnit5
 
         public void showStatement()
         {
+            Console.WriteLine("Introduce a number on base decimal to turn into binary base: ");
+            int decimalNumber = Convert.ToInt32(Console.ReadLine());
 
+             Console.WriteLine("The number {0} in binary base is: {1}", decimalNumber ,DecimalToBinary(decimalNumber));
+        }
+
+        public string DecimalToBinary(int decimalNum)
+        {
+            string result = "";
+
+                while (decimalNum > 0)
+                {
+                    if (decimalNum % 2 == 0)
+                    {
+                        result = "0" + result;
+                    }
+                    else
+                    {
+                        result = "1" + result;
+                    }
+
+                    decimalNum = (int)(decimalNum / 2);
+                }
+
+            return result;
         }
 
     }
