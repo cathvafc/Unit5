6191190f8458d561f8483c1fe810c45c210559e0
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-29 10:12:51 +0100

    Exercise 13

--- Unit5Exercises/Unit5Exercises/Exercises/Exercise13.cs
+++ Unit5Exercises/Unit5Exercises/Exercises/Exercise13.cs
@@ -6,6 +6,8 @@ namespace ExerciseUnit5
 {
     class Exercise13
     {
+        private readonly Random _random = new Random();
+        int[] example;
         public Exercise13()
         {
 
@@ -13,6 +15,26 @@ namespace ExerciseUnit5
 
         public void showStatement()
         {
+            Console.WriteLine("Introduce the length of the array");
+
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            example = new int[length];
+
+            FillArray();
+
+        }
+
+        public void FillArray()
+        {
+
+            for (int count = 0; count < example.Length; count++)
+            {
+                example[count] = _random.Next(1, 10);
+
+                Console.WriteLine("The position {0} is equal to {1}", count, example[count]);
+
+            }
 
         }
 
