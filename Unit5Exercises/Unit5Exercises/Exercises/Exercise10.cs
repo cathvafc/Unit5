2941706b3aec05c5c6ee695187aa793cc5921513
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-29 10:02:32 +0100

    Exercise 10

--- Unit5Exercises/Unit5Exercises/Exercises/Exercise10.cs
+++ Unit5Exercises/Unit5Exercises/Exercises/Exercise10.cs
@@ -6,15 +6,36 @@ namespace ExerciseUnit5
 {
     class Exercise10
     {
+        int[] example;
         public Exercise10()
         {
-
+            example = new int[11];
         }
 
         public void showStatement()
         {
 
+            Console.WriteLine("Introduce the value to calculate its multiplication table.");
+
+            Calculate(Convert.ToInt32(Console.ReadLine()));
         }
 
+        public void Calculate(int value)
+        {
+            for (int i = 0; i < example.Length; i++)
+            {
+                example[i] = value * i;
+            }
+
+            ShowContent(value);
+        }
+
+        public void ShowContent(int value)
+        {
+            for (int i = 0; i < example.Length; i++)
+            {
+                Console.WriteLine("The value of {0} * {1} is: {2}", value, i, example[i]);
+            }
+        }
     }
 }
