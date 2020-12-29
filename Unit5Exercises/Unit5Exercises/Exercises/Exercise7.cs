67663c615f3195e1a214e7e1a7c989b7b27a832c
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-29 09:28:49 +0100

    Exercise 7

--- Unit5Exercises/Unit5Exercises/Exercises/Exercise7.cs
+++ Unit5Exercises/Unit5Exercises/Exercises/Exercise7.cs
@@ -14,6 +14,40 @@ namespace ExerciseUnit5
 
         public void showStatement()
         {
+            Console.WriteLine("Please, insert how many euros do you want to convert: ");
+            int euros = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please, insert the destiny currency: Dolares, Yenes or Libras");
+            string currency = Console.ReadLine();
+
+
+            ConvertTo(euros, currency);
+        }
+
+        public void ConvertTo(int euros, string currency)
+        {
+
+            double result = 0;
+
+            if (currency == "Yenes")
+            {
+                result = euros * 129.852;
+                Console.WriteLine("{0} euros are equal to {1} Yenes.", euros, result);
+            }
+            else if (currency == "Dolares")
+            {
+                result = euros * 1.28611;
+                Console.WriteLine("{0} euros are equal to {1} Dolares.", euros, result);
+            }
+            else if(currency == "Libras")
+            {
+                result = euros * 0.86;
+                Console.WriteLine("{0} euros are equal to {1} Libras.", euros, result);
+            }
+            else
+            {
+                Console.WriteLine("You entered a wrong currency.", euros, result);
+            }
 
         }
     }
