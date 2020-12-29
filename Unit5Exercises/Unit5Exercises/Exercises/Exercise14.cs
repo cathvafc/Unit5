a7960dd76cb5e5c3ab352e4584cd5e5b4678c3db
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-29 10:18:20 +0100

    Exercise 14

--- Unit5Exercises/Unit5Exercises/Exercises/Exercise14.cs
+++ Unit5Exercises/Unit5Exercises/Exercises/Exercise14.cs
@@ -6,6 +6,8 @@ namespace ExerciseUnit5
 {
     class Exercise14
     {
+        private readonly Random _random = new Random();
+        int[] example;
         public Exercise14()
         {
 
@@ -13,7 +15,52 @@ namespace ExerciseUnit5
 
         public void showStatement()
         {
+            Console.WriteLine("Introduce the length of the array");
 
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            example = new int[length];
+
+            FillArray();
+
+            Console.WriteLine("Introduce the value to find: ");
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            if (FindValue(value))
+            {
+                Console.WriteLine("The value {0} is on the array.", value);
+            }
+            else
+            {
+                Console.WriteLine("The value {0} is not on the array.", value);
+            }
+        }
+
+        public void FillArray()
+        {
+
+            for (int count = 0; count < example.Length; count++)
+            {
+                example[count] = _random.Next(1, 10);
+
+                Console.WriteLine("The position {0} is equal to {1}", count, example[count]);
+
+            }
+
+        }
+        public bool FindValue(int value)
+        {
+            bool find = false;
+
+            for (int count = 0; count < example.Length; count++)
+            {
+                if (example[count] == value)
+                {
+                    find = true;
+                }
+                
+            }
+            return find;
         }
     }
 }
