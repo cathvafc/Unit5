2ce857a013ad29b6189f5015d59831a1308951ab
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-29 10:10:38 +0100

    Exercise 12

--- Unit5Exercises/Unit5Exercises/Exercises/Exercise12.cs
+++ Unit5Exercises/Unit5Exercises/Exercises/Exercise12.cs
@@ -6,6 +6,7 @@ namespace ExerciseUnit5
 {
     class Exercise12
     {
+        int[] example;
         public Exercise12()
         {
 
@@ -14,6 +15,38 @@ namespace ExerciseUnit5
         public void showStatement()
         {
 
+
+            example = new int[10];
+
+            FillArray();
+
+        }
+
+        public void FillArray()
+        {
+
+            for (int count = 0; count < example.Length; count++)
+            {
+                example[count] = count;
+
+            }
+
+            Average();
+
+        }
+
+        public void Average()
+        {
+
+            double result = 0;
+
+            for (int count = 0; count < example.Length; count++)
+            {
+                result += example[count];
+
+            }
+
+            Console.WriteLine("The average of all position of the array is: {0}", result/example.Length);
         }
 
     }
