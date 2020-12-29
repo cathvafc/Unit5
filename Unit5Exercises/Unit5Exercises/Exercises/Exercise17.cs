9a8cd37b643cb612e22bb0fa59fba34174f04168
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-29 11:32:14 +0100

    Exercise 17

--- Unit5Exercises/Unit5Exercises/Exercises/Exercise17.cs
+++ Unit5Exercises/Unit5Exercises/Exercises/Exercise17.cs
@@ -6,6 +6,7 @@ namespace ExerciseUnit5
 {
     class Exercise17
     {
+        int[] example;
         public Exercise17()
         {
 
@@ -14,6 +15,37 @@ namespace ExerciseUnit5
         public void showStatement()
         {
 
+            example = new int[10];
+
+            FillArray();
+
+        }
+
+        public void FillArray()
+        {
+
+            for (int count = 0; count < example.Length; count++)
+            {
+
+                Console.WriteLine("Introduce the integer value for the position {0}: ", count);
+
+                int valueRead = Convert.ToInt32(Console.ReadLine());
+
+                example[count] = valueRead;
+
+            }
+
+            ShowContent();
+
+        }
+
+        public void ShowContent()
+        {
+            
+            for (int count = 0; count < example.Length; count++)
+            {
+                Console.WriteLine("The value of the position | {0} | is | {1} | ", count, example[count]);
+            }
         }
     }
 }
