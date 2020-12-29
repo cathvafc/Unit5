1113e769872764ba56c7bb1a1d556c278962ac59
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-29 10:23:48 +0100

    Exercise 15

--- Unit5Exercises/Exercise2/Program.cs
+++ Unit5Exercises/Exercise2/Program.cs
@@ -4,9 +4,41 @@ namespace Exercise2
 {
     class Program
     {
+        private static readonly Random _random = new Random();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Introduce how many random numbers do you want: ");
+            int randomNumbers = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce the interval to create the random numbers: \n The first part of the interval:  ");
+            int lowerInterval = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Introduce the second part of the interval:  ");
+            int upperInterval = Convert.ToInt32(Console.ReadLine());
+
+
+            int iterator = 0;
+            int randomResult = 0;
+
+            while (iterator < randomNumbers)
+            {
+
+                randomResult = generateRandomNumber(lowerInterval, upperInterval);
+
+                Console.WriteLine("The random number is: {0}", randomResult);
+                iterator++;
+            }
+
+        }
+
+        static int generateRandomNumber (int min, int max)
+        {
+            int result = 0;
+
+            result = _random.Next(min, max);
+
+            return result;
         }
     }
 }
--- Unit5Exercises/Unit5Exercises/Exercises/Exercise15.cs
+++ Unit5Exercises/Unit5Exercises/Exercises/Exercise15.cs
@@ -6,6 +6,9 @@ namespace ExerciseUnit5
 {
     class Exercise15
     {
+        private readonly Random _random = new Random();
+        int[] example;
+        int[] example2;
         public Exercise15()
         {
 
@@ -13,7 +16,43 @@ namespace ExerciseUnit5
 
         public void showStatement()
         {
+            Console.WriteLine("Introduce the length of the array");
 
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            example = new int[length];
+            example2 = new int[length];
+
+            FillArray();
+
+            TurnArray();
+        }
+
+        public void FillArray()
+        {
+
+            for (int count = 0; count < example.Length; count++)
+            {
+                example[count] = _random.Next(1, 10);
+
+                Console.WriteLine("The position {0} is equal to {1}", count, example[count]);
+
+            }
+
+        }
+
+        public void TurnArray()
+        {
+            int count = example.Length - 1;
+
+            for (int i = 0; i < example.Length; i++)
+            {
+                example2[i] = example[count];
+
+                Console.WriteLine("The position {0} in the **new array** is equal to {1}", i, example2[i]);
+
+                count--;
+            }
         }
     }
 }
