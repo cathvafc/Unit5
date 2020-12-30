3ddb9a19e7e99aa19fcd6fb700ef4d0adfb82a18
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-30 10:25:22 +0100

    Exercise 21

--- Unit5Exercises/Unit5Exercises/Exercises/Exercise21.cs
+++ Unit5Exercises/Unit5Exercises/Exercises/Exercise21.cs
@@ -6,6 +6,12 @@ namespace ExerciseUnit5
 {
     class Exercise21
     {
+        private readonly Random _random = new Random();
+        int min = 1;
+        int max = 300;
+        int[] firstArray;
+        int[] secondArray;
+        int lengthArray = 0;
         public Exercise21()
         {
 
@@ -13,7 +19,62 @@ namespace ExerciseUnit5
 
         public void showStatement()
         {
+            do
+            {
+                Console.WriteLine("Introduce the length of the Arrays: ");
 
+                lengthArray = Convert.ToInt32(Console.ReadLine());
+
+                if (0 > lengthArray)
+                {
+
+                    Console.WriteLine("The value of the arrays length is incorrect. Introduce it again.");
+
+                }
+            } while (0 > lengthArray);
+
+           firstArray = new int[lengthArray];
+            secondArray = new int[lengthArray];
+
+            FillArray(firstArray);
+
+            ShowArraysContent("The array result is: ",secondArray);
+        }
+
+        public void FillArray(int[] array)
+        {
+
+            int result = 0;
+            int j = 0;
+
+            for (int count = 0; count < array.Length; count++)
+            {
+                   
+                result =  _random.Next(min, max);
+
+                array[count] = result;
+
+                if (result % 10 == 5)
+                {
+                    secondArray[j] = result;
+                    j++;
+                }
+
+            }
+        }
+
+        public void ShowArraysContent(string message, int[] array)
+        {
+
+            Console.WriteLine("{0} ", message);
+
+            for (int count = 0; count < array.Length; count++)
+            {
+                if (array[count] != 0)
+                { 
+                Console.WriteLine("The position {0} is equal to: {1}", count, array[count]);
+                }
+            }
         }
     }
 }
